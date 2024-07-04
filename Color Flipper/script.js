c87e24cd86a5850f5a664d6d254ab050d2c7028d
blooming813 <blooming.anna0813@gmail.com>
2024-07-04 01:09:46 -0400

    completed color flipper so far

---
+++ Color Flipper/script.js
@@ -0,0 +1,16 @@
+// button 
+const btn = document.getElementById('btn');
+// body
+const color = document.querySelector('.color');
+// array
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+btn.addEventListener('click', function(){
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 4);
+}
